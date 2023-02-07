6b3b9151243064f722cb7299618a431d825470eb
oliverlee96 <oliverleej@hotmail.com>
2023-02-07 14:21:09 +0000

    show post body into posts view

--- 00-starting-project/routes/blog.js
+++ 00-starting-project/routes/blog.js
@@ -33,4 +33,17 @@ router.post('/posts', async function(req, res) {
     res.redirect('/posts'); //redirect to all posts page after a new post is created
 });
 
+router.get('/posts/:id', async function (req, res) {
+    const query = `SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?`;
+    const [posts] = await db.query(query, [req.params.id]);
+
+    if (!posts || posts.length === 0 ) {
+       return res.status(404).render('404');
+    }
+
+    res.render('post-detail', { post: posts[0]});
+});
+
 module.exports = router;
